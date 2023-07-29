e5b9d2ff4c978841095391b998dc337d148027e4
MoulinaPradhan <pmoulina76@gmail.com>
2023-07-29 15:52:18 +0530

    edit/update

--- angular-task-manager/src/app/task-item/task-item.component.ts
+++ angular-task-manager/src/app/task-item/task-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TaskModel } from '../providers/tasks.state';
 import { actions } from '../providers/tasks.actions';
 import { Store } from '@ngrx/store';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-task-item',
@@ -11,17 +12,36 @@ import { Store } from '@ngrx/store';
 export class TaskItemComponent implements OnInit {
 @Input() task?:TaskModel;
 editTask: boolean = false;
+editStatus?:number;
+editTakenBy?:string;
 constructor( private store:Store){}
 
   ngOnInit(): void {
     console.log("task"+this.task!.id)
-    this.editTask= this.task!.completed;
+  
     
 }
 updateToggle(){
   this.editTask=!this.editTask;
 }
-updateTask(){
+updateTask(form : NgForm){
+  console.log(form)
+  this.editTask=!this.editTask;
+  this.store.dispatch(actions.updateTaskAction({
+
+    id:this.task!.id,
+    completed:false,
+    title:form.controls['edit-task-title'].value,
+    description:form.controls['edit-task-description'].value,
+    priority:1,
+    dueDate: new Date("5-9-20"),
+    //form.controls['edit-task-dueDate'].value,
+    status:1,
+    //  form.controls['edit-task-status'].value,
+    takenBy:form.controls['edit-task-takenBy'].value
+  }));
+ 
+ 
 
 }
 deleteTask(){
